a8e05587bc64f8776e4feffc075e70f701c21eca
Jessica <andradejsscl@gmail.com>
2022-12-20 11:26:30 -0500

    event listeners and form functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+let currentMovie = {}
+
 fetch('http://localhost:3000/movies')
     .then(resp => resp.json())
-    .then(movies => renderMovies(movies))
+    .then(movies => {
+        renderMovies(movies)
+        showMovieDetails(movies[0])
+        //handleSubmit()
+        }
+    )
 
 //DOM selectors
 const movieListNav = document.querySelector('#movie-list')
+const detailedImage = document.querySelector('#detail-image')
+const movieTitle = document.querySelector('#title')
+const releaseYear = document.querySelector('#year-released')
+const movieDescription = document.querySelector('#description')
+const movieWatched = document.querySelector('#watched')
+const bloodAmount = document.querySelector('#blood-amount')
+const amount = document.querySelector('#amount')
+const form = document.querySelector('#blood-form')
+form.addEventListener('submit', handleSubmit)
 
 //renderMovies function
 function renderMovies(movies) {
@@ -12,23 +28,30 @@ function renderMovies(movies) {
         imgTag.src = movie.image
         //console.log(imgTag)
         movieListNav.append(imgTag)
-        imgTag.addEventListener('click', showMovieDetails(movie))
+        imgTag.addEventListener('click', () => showMovieDetails(movie))
     })
 }
 
 function showMovieDetails(movie) {
-    const detailedImage = document.querySelector('detail-image')
+    currentMovie = movie
     detailedImage.src = movie.image
-    const movieTitle = document.querySelector('#title')
     movieTitle.textContent = movie.title
-    const releaseYear = document.querySelector('#year-released')
     releaseYear.textContent = movie.release_year
-    const movieDescription = document.querySelector('#description')
     movieDescription.textContent = movie.description
-    const movieWatched = document.querySelector('#watched')
     movieWatched.textContent = movie.watched ? "Watched" : "UnWatched"
-    const bloodAmount = document.querySelector('#blood-amount')
     bloodAmount.textContent = movie.blood_amount
     movieWatched.addEventListener('click', () => toggleWatchedButton(movie))
 }
 
+function toggleWatchedButton(movie) {
+    movie.watched = !movie.watched
+    movieWatched.textContent = movie.watched ? "Watched" : "UnWatched"
+}
+
+function handleSubmit(e) {
+    e.preventDefault()
+    //console.log(e)
+    currentMovie.blood_amount += parseInt(e.target[0].value)
+    amount.textContent = currentMovie.blood_amount
+
+}
